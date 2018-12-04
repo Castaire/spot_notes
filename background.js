e3b7376a3db12ffe22aa869874313d045c7d53f5
Castaire <iv.castaire@gmail.com>
2018-12-03 17:57:39 -0800

    added a bit more to OAuth process :)

--- background.js
+++ background.js
@@ -6,32 +6,63 @@ chrome.runtime.onInstalled.addListener(function(){
     console.log("you just started the extension! hurray!");
 });
 
+// USAGE:   exchanges authorization code for access token in OAuth process 
+function makeXHRExchange(authCode){
+    // TODO: 
+    
+
+
+
+    return("fluffy");
+}
+
+
 // USAGE:   initiates authorization flow
 // RETURNS: authorization code and state (although not supplied in request)
 function authorizeSpotify(){
-    const clientID = "fluffy";
     const scope = "user-follow-read";
-    const redirect =  chrome.runtime.getURL("login.html");
+    const clientID = "fluffy";
+    const redirectURI = chrome.identity.getRedirectURL("spotnotes/");
 
     let authURL = `https://accounts.spotify.com/authorize?` +
-        `=${clientID}` +
-        `&response_type=code&redirect_uri=${redirect}` +
+        `client_id=${clientID}` +
+        `&response_type=code&redirect_uri=${redirectURI}` +
         `&scope=${scope}`;
+
+    console.log(authURL);
 
     // launch authorization 
     return chrome.identity.launchWebAuthFlow({
         url: authURL,
         interactive: true
     },
-    function(responseUrl){              // TODO: 
+
+    function(responseUrl){
+        console.log("just got the responseUrl back!");
         console.log(responseUrl);
+
+        // handle failed user login
+        if(responseUrl.includes("error")){
+            chrome.runtime.sendMessage({status: "unsuccessfulLogin"});
+            alert("user login was unsuccessful");
+        }
+
+        // exchange authorization code with access token
+        authCode = responseUrl.split("=")[1];
+        console.log(authCode);
+
+        accessToken = makeXHRExchange(authCode);
+        // TODO: 
+
+        
+
     });
 }
 
 // USAGE:   handles message passing from various content scripts
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     switch(request.action){
-        
+
         case "launchOAuth":
             alert("just got a launchOAuth message!");
             authorizeSpotify();
--- popup.js
+++ popup.js
@@ -1,5 +1,26 @@
 'use strict';
 
+// USAGE:   modifies popup text according to received user-login status
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    switch(request.status){
+
+        case "sucessfulLogin":
+            notificationT.textContent = "WHOO! Successfully logged-in with Spotify!";
+            authButton.textContent = "Disconnect from Spotify'";
+            break;
+
+        case "unsucessfulLogin":
+            notificationT.textContent = "Hmmm, something happened. Wanna try again?";
+            authButton.textContent = "Reconnect with Spotify";
+            break;
+
+        default:
+            console.log("WTF is " + request.status + " , bruh ???");
+    }
+});
+
+
+
 // USAGE:   initialize Spotify user login on button-click in pop-up
 function initializeLogin(){
     chrome.runtime.sendMessage({action: 'launchOAuth'});
@@ -7,3 +28,5 @@ function initializeLogin(){
 
 let authButton = document.getElementById('userAuth');
 authButton.addEventListener('click', initializeLogin);
+
+let notificationT = document.getElementById('notificationText');
